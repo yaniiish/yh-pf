2a605f39d616b5774902327dbf208c145d75df72
yaniiish <yanis.herda14@gmail.com>
2023-03-20 01:43:07 +0100

    debut pf fc

---
+++ script.js
@@ -0,0 +1,13 @@
+const firstSection = document.querySelector('#first-section');
+const secondSection = document.querySelector('#second-section');
+let hasScrolled = false;
+
+window.addEventListener('wheel', function(event) {
+  if (event.deltaY > 0 && !hasScrolled) {
+    hasScrolled = true;
+    window.scrollTo({
+      top: secondSection.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+});
